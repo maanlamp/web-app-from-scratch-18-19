fe777c9faab1fdeb8759e46040e016fb50d9bc01
maanlamp <32102935+maanlamp@users.noreply.github.com>
2019-02-04 18:07:53 +0100

    Dag 1

--- week1/README.md
+++ week1/README.md
@@ -1,7 +1,22 @@
 # Week 1 - Hello API 🐒
 
-Doel: Data ophalen uit een API en renderen in een overzichtspagina.
+## Live demo
+O,o
 
-[Opdrachten](https://drive.google.com/open?id=1OVhWQNaCgSluYviTKKWcApkyPd23xow1PiExb8GYANM)
+## Wat is deze?
+O,o
 
-[Slides](https://drive.google.com/open?id=1Rjl9xqXoKniQSRJPdkU1O5YwWC33SJK8KiV0a-H_xZU)
+## Welke actoren zitten er in jouw applicatie? (actor diagram)
+O,o
+
+## Welke API wordt gebruikt en wat zijn de beperkingen? (rate limit)
+O,o
+
+## Hoe flowed interactie door de applicatie? (interaction diagram)
+O,o
+
+## Welke design patterns en best practices
+O,o
+
+## Wat zou je nog willen toevoegen (feature wishlist / backlog)
+O,o
---
+++ week1/public/js/app.js
@@ -0,0 +1,81 @@
+const main = document.querySelector("main");
+const endpoint = "https://musicbrainz.org/ws/2";
+
+Object.defineProperty(String.prototype, "lastChar", {
+	value: function lastChar (count = 1) {
+		return this.slice(-count);
+	},
+	configurable: false
+});
+
+function createURL (baseURL, options) {
+	const URLOptions = Object.entries(options)
+		.map(entry => entry.join("="))
+		.join("&");
+
+	return `${baseURL}?${URLOptions}`;
+}
+
+async function getAll (thing, options) {
+	const results = [];
+	let maxResults = Infinity;
+
+	while (results.length < maxResults) {
+		await fetch(createURL(`${endpoint}/${thing}`, options))
+			.then(response => response.json())
+			.then(json => {
+				maxResults = json.count;
+				results.push(...json.artists);
+			})
+			.catch(err => {throw err});
+	}
+	
+	return results;
+}
+
+/* ---------------------- */
+
+const input = document.querySelector("input");
+function scaleInput () {
+	input.style.width = `${Math.max(1, input.value.length)}ch`;
+}
+scaleInput();
+input.addEventListener("input", scaleInput);
+
+const section = document.querySelector("#artists");
+function clearArtists () {
+	while (section.lastChild) section.lastChild.remove();
+}
+
+function isOfZijn (count) {
+	return (count !== 1) ? "zijn" : "is";
+}
+
+const form = document.querySelector("form");
+const count = document.querySelector("#count");
+form.addEventListener("submit", async event => {
+	event.preventDefault();
+
+	count.innerText = "";
+
+	const options = {
+		fmt: "json",
+		query: input.value
+	};
+
+	clearArtists();
+
+	const results = (await getAll("artist", options))
+		.filter(artist => artist.name.split(" ")[0].toLowerCase() === input.value.toLowerCase())
+		.map(artist => artist.name)
+		.filter((name, i, source) => source.indexOf(name) === i);
+
+	results.forEach(name => {
+		const div = document.createElement("div");
+		div.className = "list-item";
+		div.innerText = name;
+		section.append(div);
+	});
+
+	count.innerText = `Dat ${isOfZijn(results.length)} er ${results.length}!`;
+});
